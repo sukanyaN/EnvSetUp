33bf4961dd0c47babb45814ea61b31fc55173bcf
sukanyaN <rajansuki@gmail.com>
2015-10-04 16:45:43 -0500

    fixed Image path

--- install-env.sh
+++ install-env.sh
@@ -5,7 +5,7 @@ sudo apt-get install  -y  apache2 git
 
 git clone https://github.com/sukanyaN/AppSetUp.git
 
-mv ./AppSetup/images /var/www/html/images
+mv ./AppSetup/images/IIT_Scarlet_Hawks.png /var/www/html/images/
 mv ./AppSetUp/index.html /var/www/html
 mv ./AppSetUp/Page2.html /var/www/html
 
