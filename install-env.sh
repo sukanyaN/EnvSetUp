606d6b7aa88c3e29aec03b96203a3eb8fc20cc0c
Sukanya Nagarajan <rajansuki@gmail.com>
2015-10-04 15:45:25 -0500

    Added Install-env.sh which containes Pull commands and Install commands to apache2

---
+++ install-env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install  -y  apache2 git
+
+git clone https://github.com/sukanyaN/AppSetUp.git
+
+mv ./AppSetup/images /var/www/html/images
+mv ./AppSetUp/Page1.html /var/www/html
+mv ./AppSetUp/Page2.html /var/www/html
+
+
+
+echo "Hello!" > /tmp/hello.txt
