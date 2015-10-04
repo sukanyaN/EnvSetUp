0e0345c384771afe4dfc7c34cfa25d389a9b8a0e
Sukanya Nagarajan <rajansuki@gmail.com>
2015-10-04 17:24:34 -0500

    Fixing issue with image Url

--- install-env.sh
+++ install-env.sh
@@ -6,7 +6,7 @@ sudo apt-get install  -y  apache2 git
 git clone https://github.com/sukanyaN/AppSetUp.git
 
 mkdir /var/www/html/images
-mv ./AppSetup/images /var/www/html/images
+cp ./AppSetup/images/IIT_Scarlet_Hawks.svg.png /var/www/html/images/
 mv ./AppSetUp/index.html /var/www/html
 mv ./AppSetUp/Page2.html /var/www/html
 
