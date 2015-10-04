319ff43f9d030bcc38453e67f12b74221ce24a09
Sukanya Nagarajan <rajansuki@gmail.com>
2015-10-04 18:15:25 -0500

    fixed typo

--- install-env.sh
+++ install-env.sh
@@ -5,10 +5,9 @@ sudo apt-get install  -y  apache2 git
 
 git clone https://github.com/sukanyaN/AppSetUp.git
 
-mkdir /var/www/html/images > tmp0.txt
-mv ./AppSetup/images /var/www/html/images > tmp.txt
-mv ./AppSetUp/index.html /var/www/html > tmp2.txt
-mv ./AppSetUp/Page2.html /var/www/html > tmp3.txt
+mv ./AppSetUp/images /var/www/html/images 
+mv ./AppSetUp/index.html /var/www/html 
+mv ./AppSetUp/Page2.html /var/www/html 
 
 
 
