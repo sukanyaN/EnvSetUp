db6a2c5d6f05df09cbc8e506766610bb5f9a5a85
Sukanya Nagarajan <rajansuki@gmail.com>
2015-10-31 12:53:00 -0500

    modified path of php files

--- install-env.sh
+++ install-env.sh
@@ -4,10 +4,10 @@ sudo apt-get update -y
 sudo apt-get install  -y  apache2 git php5-curl mysql-client curl
 
 git clone https://github.com/sukanyaN/AppSetUp.git
+git clone https://github.com/sukanyaN/itmo-544-final.git
 
 mv ./AppSetUp/images /var/www/html/images 
-mv ./AppSetUp/index.html /var/www/html 
-mv ./AppSetUp/*.php /var/www/html 
+mv ./itmo-544-final/*.php /var/www/html 
 
 curl -sS https://getcomposer.org/installer |php
 php composer.phar require aws/aws-sdk-php
