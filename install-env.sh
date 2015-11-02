a988c99eeb9ade57cc5736007eaa719fe99925f3
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-02 17:49:41 -0600

    modified file to include sudo keyword

--- install-env.sh
+++ install-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install  -y  apache2 git php5-curl mysql-client curl
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/sukanyaN/AppSetUp.git
 git clone https://github.com/sukanyaN/itmo-544-final.git
@@ -9,9 +9,13 @@ git clone https://github.com/sukanyaN/itmo-544-final.git
 mv ./AppSetUp/images /var/www/html/images 
 mv ./itmo-544-final/*.php /var/www/html 
 
-curl -sS https://getcomposer.org/installer |php
-php composer.phar require aws/aws-sdk-php
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
-mv vendor /var/www/html
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
 
 echo "Hello!" > /tmp/hello.txt
+
+
+
