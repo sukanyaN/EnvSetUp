c1b5c25a718116a1a370291a3274cbccc7233ad7
Sukanya Nagarajan <rajansuki@gmail.com>
2015-11-02 18:21:36 -0600

    modified file to include sudo keyword

--- install-env.sh
+++ install-env.sh
@@ -6,8 +6,8 @@ sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 git clone https://github.com/sukanyaN/AppSetUp.git
 git clone https://github.com/sukanyaN/itmo-544-final.git
 
-mv ./AppSetUp/images /var/www/html/images 
-mv ./itmo-544-final/*.php /var/www/html 
+sudo mv ./AppSetUp/images /var/www/html/images 
+sudo mv ./itmo-544-final/*.php /var/www/html 
 
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
